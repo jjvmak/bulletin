717411804a1dbeca8e5a821c489c7a99581a3354
Joonas Mäkinen <makinen.jns@gmail.com>
2017-12-12 16:22:48 +0200

    Connection to mongodb via MLab works now. For the security reasons external manager class had to be created, which holds connection information. Manager class is added to gitignore and won't be uploaded to GitHub.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
+'use strict';
+const Manager = require('./Manager.js'); //for the security reasons this class has been added to gitignore.
+let manager = new Manager();
+
 const app = require('express')();
 const cors = require('cors')
 const bodyParser = require('body-parser');
+
 
 //Enable CORS
 app.use(cors());
@@ -26,10 +31,12 @@ const server = app.listen(port, () => {
 var mongo = require('mongodb');
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:8081/db";
+var url = manager.dburl;
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
   db.close();
 })
+
+
