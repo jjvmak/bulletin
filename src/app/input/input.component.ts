8e729b7ba51a666173157cf5a710e84cea40c009
Joonas Mäkinen <makinen.jns@gmail.com>
2017-12-19 09:40:32 +0200

    Removed uneccesary code.

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -10,21 +10,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class InputComponent implements OnInit {
   @ViewChild('message') messageForm: NgForm;
-  ln: number;
-  messages: string [] = [];
+ 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://localhost:8080/messages', {observe: 'response'})
-      .subscribe(resp => {
-        this.ln = Object.keys(resp.body).length;
 
-        for (let i = 0; i < this.ln; i++) {
-          this.messages.push(resp.body[i].message);
-          console.log(this.messages[i]);
-        }
-
-      });
   }
 
   onSubmit() {
